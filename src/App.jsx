7a187188586e09ba8c460b30862544be77b84107
“Samuel <amagbakhensamuel@gmail.com>
2024-08-08 03:01:29 +0100

    Done: Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css';
 import LandingPage from './modules/landing-page/LandingPage';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <>
+      <TawkMessengerReact
+        propertyId={import.meta.env.VITE_TAWK_TO_PROPERTY_ID}
+        widgetId={import.meta.env.VITE_TAWKTO_WIDGET_ID}
+      />
+      <LandingPage />;
+    </>
+  );
 }
 
 export default App;
